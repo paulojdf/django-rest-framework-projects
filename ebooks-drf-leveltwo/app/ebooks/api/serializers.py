d9ea7b57178c9cb746dc62b93b6e692e626c99bb
Paulo Fernandes <paulojdf27@gmail.com>
2022-04-22 15:03:22 +0100

    Add GenericAPIView feature.

--- ebooks-drf-leveltwo/app/ebooks/api/serializers.py
+++ ebooks-drf-leveltwo/app/ebooks/api/serializers.py
@@ -2,15 +2,15 @@ from rest_framework import serializers
 from ebooks.models import Ebook, Review
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
 class EbookSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Ebook
-        fields = "__all__"
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
         fields = "__all__"
--- ebooks-drf-leveltwo/app/ebooks/api/views.py
+++ ebooks-drf-leveltwo/app/ebooks/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import mixins
 
-# Create your views here.
+from ebooks.models import Ebook
+from ebooks.api.serializers import EbookSerializer
+
+
+class EbookListCreateAPIView(
+    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+):
+
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
