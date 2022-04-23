3ee64af7a8083c5d163b3b776a5ea23ec00a2a01
Paulo Fernandes <paulojdf27@gmail.com>
2022-04-24 00:43:55 +0100

    Add pagination feature

--- ebooks-drf-leveltwo/app/ebooks/api/views.py
+++ ebooks-drf-leveltwo/app/ebooks/api/views.py
@@ -3,21 +3,21 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 
 # from rest_framework import mixins
+from ebooks.models import Ebook, Review
 from ebooks.api.permissions import (
     IsAdminUserOrReadOnly,
     IsReviewAuthorOrReadOnly,
 )
-
-from ebooks.models import Ebook, Review
-
+from ebooks.api.pagination import SmallSetPagination
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
 
 
 class EbookListCreateAPIView(generics.ListCreateAPIView):
 
-    queryset = Ebook.objects.all()
+    queryset = Ebook.objects.all().order_by("id")
     serializer_class = EbookSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+    pagination_class = SmallSetPagination
 
 
 class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
--- ebooks-drf-leveltwo/app/ebooksapi/settings.py
+++ ebooks-drf-leveltwo/app/ebooksapi/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = "static/"
 #         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
 #     ]
 # }
+
+# REST_FRAMEWORK = {
+#     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+#     "PAGE_SIZE": 3,
+# }
